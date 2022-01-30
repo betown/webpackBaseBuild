e72784d9c7efd3115556fe919687c927a8273fd8
betown <joe.gomezr@gmail.com>
2022-01-30 14:00:42 -0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          context: './src/',
+          from: "**/*.html",
+          force: true
+        }
+      ],
+    }),
+    new BrowserSyncPlugin({
+      files: ['*.js', '*.html'],
+      host: 'localhost',
+      port: 3000,
+      proxy: 'http://localhost:8080'
+    }),
+
+  ]
+}
